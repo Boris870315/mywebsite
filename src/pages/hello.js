d06d53792189334af11ff32b6ea6b7758cd3b850
Boris Chen <chenbaihan97@gmail.com>
2024-03-19 13:28:27 +1100

    works updated

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from "react-slick";
+import './hello.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -11,33 +12,38 @@ import skillsData from '../assets/skill.json';
 
 function HelloPage() {
   const settings = {
-    dots: true, 
-    infinite: true, 
-    speed: 500, 
-    slidesToShow: 3, 
-    slidesToScroll: 1, 
-    autoplay: true, 
-    autoplaySpeed: 2000, 
-    responsive: [ 
+    dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    arrows: false, 
+    responsive: [
       {
-        breakpoint: 1024, 
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: true,
+          arrows: false
         }
       },
       {
-        breakpoint: 600, // 屏幕宽度小于600px时的设置
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 1
+          initialSlide: 1,
+          arrows: false 
         }
       }
     ]
   };
+  
 
   return (
     <div className='container'>
@@ -46,18 +52,18 @@ function HelloPage() {
       <div className='intext'>
         <h1> Skills </h1>
         <div className="slider-container">
-      <Slider {...settings}>
-        {skillsData.map((skill) => (
-            <div key={skill.id}>
-              <SkillPiece 
-                skills={skill.skills} 
-                imgPath={require(`../assets/skillsImg/${skill.skills}.png`)} 
-                imgAlt={skill.imgAlt} 
-              />
-            </div>
-          ))}
-      </Slider>
-    </div>
+          <Slider {...settings}>
+            {skillsData.map((skill) => (
+              <div key={skill.id}>
+                <SkillPiece 
+                  skills={skill.skills} 
+                  imgPath={require(`../assets/skillsImg/${skill.skills}.png`)} 
+                  imgAlt={skill.imgAlt} 
+                />
+              </div>
+             ))}
+          </Slider>
+        </div>
         
       </div>
       <Footer />
